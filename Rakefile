a72934e6df504e3df2ef00b586f178c8a0e1be44
Tom Smith <tom@solrr.net>
2016-04-24 10:38:05 -0600

    Init

---
+++ Rakefile
@@ -0,0 +1,3 @@
+task :watch do
+  `watchify -t [ babelify --presets [ react ] ] js/main.js -o js/bundle.js -v`
+end
---
+++ js/main.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello WORLD</h1>,
+  document.getElementById('main')
+);
+
